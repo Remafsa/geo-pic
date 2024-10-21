797abff2b9c16a27317f80b3ee1c308c0e3fa21f
mohammed alshawaf <mhmdalshawaf@gmail.com>
2024-10-21 14:04:39 +0300

    Added new file main.py for api app

--- recommendation_system/ml_logic/__init__.py
+++ recommendation_system/ml_logic/__init__.py
@@ -3,3 +3,4 @@
 from .downloader import download_images, extract_image_urls_from_json,download_images_from_file
 from .feature_extractor import extract_features
 from .recommender import find_cosine_similarity,find_similar_images,print_similar_images
+#d
